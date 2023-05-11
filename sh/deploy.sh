c2f1b998a1131059cba81d6f7af09c2359d89a05
yanh <891367701@qq.com>
2023-05-11 20:32:09 +0800

    up

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -6,34 +6,42 @@ tagImageName=$3
 port=$4
 containerport=$5
 
-echo "$tagImageName"
+echo "$tagImageName" > "/opt/log.txt"
 
 #查询容器是否存在，存在则删除
 containerId=`docker ps -a | grep -w ${project_name} | awk '{print $1}'`
 if [ "$containerId" != "" ] ; then
     #停掉容器
-    docker stop $containerId
+    sudo docker stop $containerId
     #删除容器
-    docker rm $containerId
-    echo "成功删除容器"
+    sudo docker rm $containerId
+    echo "成功删除容器1" >> "/opt/log.txt"
 fi
 
 #模糊删除镜像
 imageId=`docker images | grep -w ${tagImageName} | awk '{print $3}'`
 if [ "$imageId" != "" ] ; then
     #删除镜像       
-    docker rmi -f $imageId
-    echo "成功删除镜像"
+    sudo docker rmi -f $imageId
+    echo "成功删除镜像2" >> "/opt/log.txt"
 #删除none镜像
     
 fi
 # 登录Harbor私服
-docker login -u admin -p Harbor12345 $harbor_url
+sudo docker login -u harbor -p 1234567890Yh! $harbor_url
+
+echo "docker login -u harbor -p 1234567890Yh! $harbor_url" >> "/opt/log.txt"
+
 # 下载镜像
-docker pull $tagImageName
+sudo docker pull $tagImageName
+
+echo "docker pull $tagImageName" >> "/opt/log.txt"
+
 # 启动容器
-docker run -d -p $port:$containerport --name $project_name $tagImageName
+sudo docker run -d -p $port:$containerport --name $project_name $tagImageName
 
-docker rmi `docker images|grep none| awk '{print $3}'`
+echo "docker run -d -p $port:$containerport --name $project_name $tagImageName" >> "/opt/log.txt"
 
-echo "容器启动成功"
+sudo docker rmi `docker images|grep none| awk '{print $3}'`
+
+echo "容器启动成功!!!" >> "/opt/log.txt"
